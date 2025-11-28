6cc854ee87afa9aeb3c499efad276d3217acf271
Qutub Ahmed <qutubahmedkhan@outlook.com>
2025-11-28 19:26:10 +0500

    remove button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,9 +17,9 @@ export default function Hero() {
             <a href="#contact" className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
               Contact Me
             </a>
-            <a href="" className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+            {/* <a href="" className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
               Button
-            </a>
+            </a> */}
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
